a825d21d0c3bdd25488c04d0095add9f87d3dc7e
Ritam  Majumdar <hrickmajumdar@gmail.com>
2021-09-19 22:34:13 +0530

    Create Utils.py

---
+++ Utils.py
@@ -0,0 +1,8 @@
+def sample_images(batches_done):
+    """Saves a generated sample from the validation set"""
+    imgs = next(iter(val_dataloader))
+    real_A = Variable(imgs["B"].type(Tensor))
+    real_B = Variable(imgs["A"].type(Tensor))
+    fake_B = generator(real_A)
+    img_sample = torch.cat((real_A.data, fake_B.data, real_B.data), -2)
+    save_image(img_sample, "images/%s/%s.png" % (opt.dataset_name, batches_done), nrow=5, normalize=True)
