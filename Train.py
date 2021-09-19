95f8a467a5399f3d494441f33d35742a5bed3313
Ritam  Majumdar <hrickmajumdar@gmail.com>
2021-09-19 22:33:49 +0530

    Create Train.py

---
+++ Train.py
@@ -0,0 +1,99 @@
+import argparse
+import os
+import numpy as np
+import math
+import itertools
+import time
+import datetime
+import sys
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+from models import *
+from datasets import *
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+def Train():
+    prev_time = time.time()
+
+    for epoch in range(opt.epoch, opt.n_epochs):
+        for i, batch in enumerate(dataloader):
+
+            # Model inputs
+            real_A = Variable(batch["B"].type(Tensor))
+            real_B = Variable(batch["A"].type(Tensor))
+
+            # Adversarial ground truths
+            valid = Variable(Tensor(np.ones((real_A.size(0), *patch))), requires_grad=False)
+            fake = Variable(Tensor(np.zeros((real_A.size(0), *patch))), requires_grad=False)
+
+            optimizer_G.zero_grad()
+
+            # GAN loss
+            fake_B = generator(real_A)
+            pred_fake = discriminator(fake_B, real_A)
+            loss_GAN = criterion_GAN(pred_fake, valid)
+            # Pixel-wise loss
+            loss_pixel = criterion_pixelwise(fake_B, real_B)
+
+            # Total loss
+            loss_G = loss_GAN + lambda_pixel * loss_pixel
+
+            loss_G.backward()
+
+            optimizer_G.step()
+
+            optimizer_D.zero_grad()
+
+            # Real loss
+            pred_real = discriminator(real_B, real_A)
+            loss_real = criterion_GAN(pred_real, valid)
+
+            # Fake loss
+            pred_fake = discriminator(fake_B.detach(), real_A)
+            loss_fake = criterion_GAN(pred_fake, fake)
+
+            # Total loss
+            loss_D = 0.5 * (loss_real + loss_fake)
+
+            loss_D.backward()
+            optimizer_D.step()
+
+            # Determine approximate time left
+            batches_done = epoch * len(dataloader) + i
+            batches_left = opt.n_epochs * len(dataloader) - batches_done
+            time_left = datetime.timedelta(seconds=batches_left * (time.time() - prev_time))
+            prev_time = time.time()
+
+            # Print log
+            sys.stdout.write(
+                "\r[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f, pixel: %f, adv: %f] ETA: %s"
+                % (
+                    epoch,
+                    opt.n_epochs,
+                    i,
+                    len(dataloader),
+                    loss_D.item(),
+                    loss_G.item(),
+                    loss_pixel.item(),
+                    loss_GAN.item(),
+                    time_left,
+                )
+            )
+
+            # If at sample interval save image
+            if batches_done % opt.sample_interval == 0:
+                sample_images(batches_done)
+
+        if opt.checkpoint_interval != -1 and epoch % opt.checkpoint_interval == 0:
+            # Save model checkpoints
+            torch.save(generator.state_dict(), "saved_models/%s/generator_%d.pth" % (opt.dataset_name, epoch))
+            torch.save(discriminator.state_dict(), "saved_models/%s/discriminator_%d.pth" % (opt.dataset_name, epoch))
